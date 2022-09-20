aa6e0da367fdaaf8fde8d5e00517ff4d4bd6b8da
MaxYashch <maxyashch@gmail.com>
2022-09-20 14:09:00 +0300

    created getArgs helper function

--- weather.js
+++ weather.js
@@ -2,10 +2,17 @@
 import { getArgs } from './helpers/args.js'
 
 const initCLI = () => {
-    // console.log('start');
-    // console.log(process.argv);
     const args = getArgs(process.argv);
     console.log(args);
+    if (args.h) {
+        // display help
+    }
+    if (args.s) {
+        // save city
+    }
+    if (args.t) {
+        // manage token
+    }
 }
 
 initCLI();
